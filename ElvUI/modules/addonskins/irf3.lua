61316797e8198671b6c52eb4ddc0707126ff8057
JaeHyeong Sim <escorsian@naver.com>
2011-09-16 10:29:56 +0900

    IRF3 Skin test

---
+++ ElvUI/modules/addonskins/irf3.lua
@@ -0,0 +1,5 @@
+local E, C, L, DB = unpack(ElvUI) -- Import Functions/Constants, Config, Locales
+
+if not IsAddOnLoaded("InvenRaidFrames3") then print("인벤레이드프레임3 스킨 로드되지 않음") return end
+
+print("인벤레이드프레임3 스킨 로드됨") 
